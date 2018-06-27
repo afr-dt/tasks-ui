7290566c7021b16388ae27fae667272c0f5d9683
aleks <aleksgnu@gmail.com>
2018-06-27 02:00:51 -0500

    Refactoring task list :tada:

--- components/TasksList/index.js
+++ components/TasksList/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import { gql } from 'apollo-boost';
-import EditTask from '../EditTask';
+// ReactIcons
 import Edit from 'react-icons/lib/fa/pencil';
 import Show from 'react-icons/lib/fa/eye';
 import Delete from 'react-icons/lib/md/delete-forever';
+import IsDone from 'react-icons/lib/md/done-all';
+import Prog from 'react-icons/lib/go/tools';
+// Components
 import ErrorMessage from '../ErrorMessage';
+import ListLoader from '../loader/ListLoader';
+import EditTask from '../EditTask';
+
 import {
   Card,
   Button,
@@ -16,11 +22,9 @@ import {
   Col,
   CardSubtitle,
   Container,
-  ButtonGroup,
   CardFooter,
   CardHeader
 } from 'reactstrap';
-import ListLoader from '../loader/ListLoader';
 
 const TaskQuery = gql`
   query tasks {
@@ -41,17 +45,11 @@ const TaskQuery = gql`
   }
 `;
 
-class Tasks extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
+class Task extends React.Component {
   render() {
-    const { data } = this.props;
-    const { loading, tasks, error } = data;
-    console.log('T A S K S...', tasks);
+    const { item } = this.props;
 
-    let options = {
+    const options = {
       month: 'short',
       day: 'numeric'
     };
@@ -65,22 +63,91 @@ class Tasks extends React.Component {
       );
     };
 
+    const DateTimeFormatUpdated = daytime => {
+      return (
+        'Editado el ' +
+        daytime.toLocaleDateString('es-MX', options) +
+        ' ' +
+        daytime.toLocaleTimeString()
+      );
+    };
+
+    let task_update;
+    if (item.node.updated === null) {
+      task_update = <small />;
+    } else {
+      task_update = (
+        <small className="text-muted">
+          {DateTimeFormatUpdated(new Date(item.node.updated))}
+        </small>
+      );
+    }
+
     const InProgressOrFinished = in_progress => {
       if (in_progress === true) {
         in_progress = (
           <Button color="info" size="sm">
-            En curso
+            En curso <Prog />
           </Button>
         );
       } else {
         in_progress = (
           <Button color="danger" size="sm">
-            Finalizada
+            Finalizada <IsDone />
           </Button>
         );
       }
       return in_progress;
     };
+
+    return (
+      <Col key={item.node.id} sm="3" className="text-center">
+        <Card>
+          <small> {item.node.task_id} </small>
+          <CardHeader>
+            <CardTitle>{item.node.title}</CardTitle>
+          </CardHeader>
+          <CardBody>
+            <CardText>{item.node.description}</CardText>
+            <CardText>Tiempo restante {item.node.timer}</CardText>
+            <CardText>
+              <Button color="success" size="sm">
+                <Edit />
+              </Button>
+              <Button color="" size="sm">
+                <Show />
+              </Button>
+              <Button color="danger" size="sm">
+                <Delete />
+              </Button>
+            </CardText>
+          </CardBody>
+          <CardFooter>
+            {InProgressOrFinished(item.node.status)}
+            <CardText>
+              <small className="text-muted">
+                {DateTimeFormatCreated(new Date(item.node.created))}
+              </small>
+              <br />
+              {task_update}
+            </CardText>
+          </CardFooter>
+        </Card>
+        <br />
+      </Col>
+    );
+  }
+}
+
+class Tasks extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { data } = this.props;
+    const { loading, tasks, error } = data;
+    // console.log('T A S K S...', tasks);
 
     if (loading) {
       return (
@@ -109,40 +176,7 @@ class Tasks extends React.Component {
         <Container>
           <br />
           <Row>
-            {tasks.edges.map(task => (
-              <Col key={task.node.id} sm="3" className="text-center">
-                <Card body>
-                  <CardHeader>
-                    <CardTitle>{task.node.title}</CardTitle>
-                  </CardHeader>
-                  <CardBody>
-                    <CardText>{task.node.description}</CardText>
-                    <CardText>Tiempo restante {task.node.timer}</CardText>
-                    <CardText>
-                      <Button color="success" size="sm">
-                        <Edit />
-                      </Button>
-                      <Button color="" size="sm">
-                        <Show />
-                      </Button>
-                      <Button color="danger" size="sm">
-                        <Delete />
-                      </Button>
-                    </CardText>
-                  </CardBody>
-                  <CardFooter>
-                    {InProgressOrFinished(task.node.status)}
-                    <CardText>
-                      <small className="text-muted">
-                        {DateTimeFormatCreated(new Date(task.node.created))}
-                      </small>
-                    </CardText>
-                    {/* <EditTask/> */}
-                  </CardFooter>
-                </Card>
-                <br />
-              </Col>
-            ))}
+            {tasks.edges.map((item, index) => <Task item={item} key={index} />)}
           </Row>
         </Container>
       );
